928ae1cc6a65e23e71799fee933c2a03f79fb65e
Ryan Kroon <rykroon.tech@gmail.com>
2020-09-28 19:26:02 -0400

    added mongoengine

--- src/models.py
+++ src/models.py
@@ -1,19 +1,63 @@
-import pymongo
+from datetime import datetime
+import locale
+import pytz
+import uuid
+from mongoengine import Document
+from mongoengine.errors import ValidationError
+from mongoengine.fields import BooleanField, DateTimeField, EmailField, ListField, StringField, URLField, UUIDField
+
+class BaseModel(Document):
+    meta = {
+        'abstract': True
+    }
+
+    uuid = UUIDField(required=True, default=uuid.uuid4, unique=True)
+    date_created = DateTimeField(required=True, default=datetime.utcnow)
+    date_updated = DateTimeField(required=True)
+
+    def clean(self):
+        self.date_created = datetime.utcnow()
 
 
-class BaseModel:
-    collection = None
-
-    def delete(self):
-        pass
-
-    def save(self):
-        pass
+CLIENT_TYPES = ('confidential', 'credentialed', 'public')
+CLIENT_PROFILES = ('web application', 'browser-based application', 'native application')
+CLIENT_TYPE_PROFILE_MAPPING = {
+    'confidential': ('web application', ),
+    'credentialed': ('web application', ),
+    'public': ('browser-based application', 'native application')
+}
 
 
 class Client(BaseModel):
-    pass
+    type = StringField(required=True, choices=CLIENT_TYPES)
+    profile = StringField(required=True, choices=CLIENT_PROFILES)
+    secret = StringField()
+    redirect_uri = ListField(field=URLField)
+
+    def clean(self):
+        super().clean()
+        valid_profiles = CLIENT_TYPE_PROFILE_MAPPING.get(self.type, [])
+        if self.profile not in valid_profiles:
+            raise ValidationError("Invalid 'profile'")
 
 
 class User(BaseModel):
-    pass
+    name = StringField()
+    given_name = StringField()
+    family_name = StringField()
+    middle_name = StringField()
+    nickname = StringField()
+    preferred_username = StringField()
+    profile = URLField()
+    picture = URLField()
+    website = URLField()
+    email = EmailField()
+    email_verified = BooleanField(default=False)
+    gender = StringField(choices=('male', 'female'))
+    birthdate = DateTimeField()
+    zoneinfo = StringField(choices=pytz.all_timezones)
+    locale = StringField(choices=list(locale.locale_alias.keys()))
+    phone_number = StringField()
+    phonenumber_verified = BooleanField(default=False)
+    #address = 
+    #updated_at = 
