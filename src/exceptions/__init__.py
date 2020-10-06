024955e4a072817bb33430a05edb7d586905d989
Ryan Kroon <rykroon.tech@gmail.com>
2020-10-05 21:03:07 -0400

    idk

--- src/exceptions/__init__.py
+++
@@ -1,1 +0,0 @@
-from baseexceptions import TokenError, AuthorizationError
--- src/models.py
+++ src/models.py
@@ -1,10 +1,14 @@
+import crypt
 from datetime import datetime
+from hashlib import sha256
 import locale
 import pytz
+import secrets
 import uuid
 from mongoengine import Document
 from mongoengine.errors import ValidationError
 from mongoengine.fields import BooleanField, DateTimeField, EmailField, ListField, StringField, URLField, UUIDField
+
 
 class BaseModel(Document):
     meta = {
@@ -19,12 +23,23 @@ class BaseModel(Document):
         self.date_created = datetime.utcnow()
 
 
-CLIENT_TYPES = ('confidential', 'credentialed', 'public')
-CLIENT_PROFILES = ('web application', 'browser-based application', 'native application')
-CLIENT_TYPE_PROFILE_MAPPING = {
-    'confidential': ('web application', ),
-    'credentialed': ('web application', ),
-    'public': ('browser-based application', 'native application')
+CONFIDENTIAL = 'confidential'
+PUBLIC = 'public'
+CLIENT_TYPES = (CONFIDENTIAL, PUBLIC)
+
+WEB_APPLICATION = 'web application'
+BROWSER_BASED_APPLICATION = 'browser-based application'
+NATIVE_APPLICATION = 'native application'
+CLIENT_PROFILES = (
+    WEB_APPLICATION, 
+    BROWSER_BASED_APPLICATION, 
+    NATIVE_APPLICATION
+)
+
+CLIENT_PROFILE_TYPE_MAPPING = {
+    WEB_APPLICATION:            CONFIDENTIAL,
+    BROWSER_BASED_APPLICATION:  PUBLIC,
+    NATIVE_APPLICATION:         PUBLIC
 }
 
 
@@ -36,12 +51,22 @@ class Client(BaseModel):
 
     def clean(self):
         super().clean()
-        valid_profiles = CLIENT_TYPE_PROFILE_MAPPING.get(self.type, [])
-        if self.profile not in valid_profiles:
-            raise ValidationError("Invalid 'profile'")
+        self.type = CLIENT_PROFILE_TYPE_MAPPING.get(self.profile)
+        if self.type == CONFIDENTIAL:
+            self.secret = secrets.token_urlsafe()
+
+    def is_public(self):
+        return self.type == PUBLIC
+
+
+def mksalt():
+    return crypt.mksalt(crypt.METHOD_SHA256)
 
 
 class User(BaseModel):
+    salt = StringField(required=True, default=mksalt)
+    password = StringField(required=True)
+
     name = StringField()
     given_name = StringField()
     family_name = StringField()
@@ -51,13 +76,26 @@ class User(BaseModel):
     profile = URLField()
     picture = URLField()
     website = URLField()
-    email = EmailField()
-    email_verified = BooleanField(default=False)
     gender = StringField(choices=('male', 'female'))
     birthdate = DateTimeField()
     zoneinfo = StringField(choices=pytz.all_timezones)
     locale = StringField(choices=list(locale.locale_alias.keys()))
+    #updated_at = 
+    
+    email = EmailField()
+    email_verified = BooleanField(default=False)
+
     phone_number = StringField()
     phonenumber_verified = BooleanField(default=False)
+
     #address = 
-    #updated_at = 
+
+
+    def set_password(self, password):
+        salted_password = "{}{}".format(password, self.salt)
+        self.password = sha256(salted_password.encode()).hexdigest()
+
+    def check_password(self, password):
+        salted_password = "{}{}".format(password, self.salt)
+        hashed_password = sha256(salted_password.encode()).hexdigest()
+        return self.password == hashed_password
