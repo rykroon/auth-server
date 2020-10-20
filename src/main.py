4eeada96b53073bda992c64d31a1132fad355534
Ryan Kroon <rykroon.tech@gmail.com>
2020-10-19 20:18:49 -0400

    removed error_handlers

--- src/main.py
+++ src/main.py
@@ -1,11 +1,9 @@
 from flask import Flask
-from error_handlers import oauth_error_handler
 from exceptions import OAuthError
 
 
 def create_app():
     app = Flask(__name__)
-    app.register_error_handler(OAuthError, oauth_error_handler)
     return app
 
 
