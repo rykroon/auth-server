78b4a18ecad37ebb45245b40f9d5edcae86d3353
Ryan Kroon <rykroon.tech@gmail.com>
2020-12-28 20:39:27 -0500

    pulled logic from flaask_api_template

--- src/main.py
+++ src/main.py
@@ -1,22 +1,27 @@
 from flask import Flask, jsonify
-from werkzeug.exceptions import HTTPException
+from utils import JSONEncoder, get_redis_client, error_handlers
 
 
 def create_app():
     app = Flask(__name__)
+    app.json_encoder = JSONEncoder
 
-    @app.errorhandler(HTTPException)
-    def http_exception_handler(e):
-        return jsonify(
-            error=e.name,
-            error_description=e.description
-        ), e.code
+    for exc, handler in error_handlers.items():
+        app.register_error_handler(exc, handler)
+
+    @app.before_request
+    def before_request():
+        g.redis_client = get_redis_client()
+
+    @app.route('/healthz', methods=['GET'])
+    def health():
+        return "OK"
 
     return app
 
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
 
 
