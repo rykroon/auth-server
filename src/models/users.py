64f451d709d9f36299ebe601546fba82de45913e
Ryan Kroon <rykroon.tech@gmail.com>
2020-12-29 23:14:28 -0500

    added first_name, last_name, client_id

--- src/models/users.py
+++ src/models/users.py
@@ -1,15 +1,18 @@
 import crypt
 from hashlib import sha256
-from mongoengine.fields import BooleanField, EmailField, StringField
-from .base import BaseModel
+from mongoengine.fields import BooleanField, EmailField, ObjectIdField, StringField
+from .base import BaseDocument
 
 
 def mksalt():
     return crypt.mksalt(crypt.METHOD_SHA256)
 
 
-class User(BaseModel):
+class User(BaseDocument):
     username = StringField()
+
+    first_name = StringField()
+    last_name = StringField()
     
     email = EmailField()
     email_verified = BooleanField(default=False)
@@ -21,6 +24,12 @@ class User(BaseModel):
     password = StringField(required=True)
 
     is_active = BooleanField(default=True)
+
+    client_id = ObjectIdField()
+
+    @property
+    def client(self):
+        pass
 
     def set_password(self, password):
         salted_password = "{}{}".format(password, self.salt)
