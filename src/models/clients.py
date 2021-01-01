38a81ad0bdcf863ea31d4a95f28cf21eeab4a75d
Ryan Kroon <rykroon.tech@gmail.com>
2021-01-01 15:51:54 -0500

    added user and application to the access token. default access token expiration is 15 minutes, default refresh token expiration is 30 days

--- src/models/clients.py
+++ src/models/clients.py
@@ -1,13 +1,20 @@
 import secrets
-from mongoengine.fields import StringField
+from mongoengine.fields import IntField, StringField
 from .base import BaseDocument
 
 
 def generate_secret():
     return secrets.token_urlsafe()
 
+FIFTEEN_MINUTES = 60 * 15
+THIRTY_DAYS = 60 * 60 * 24 * 30
+
 
 class Client(BaseDocument):
     name = StringField(required=True)
     description = StringField(required=True)
     secret = StringField(default=generate_secret)
+
+    jwt_access_token_expires = IntField(default=FIFTEEN_MINUTES)
+    jwt_refresh_token_expires = IntField(default=THIRTY_DAYS)
+    
--- src/utils/tokens.py
+++ src/utils/tokens.py
@@ -4,20 +4,21 @@ import uuid
 import jwt
 
 
+#These global values should be able to be configured at the client level
 jwt_secret_key = os.getenv('JWT_SECRET_KEY')
-jwt_access_token_expires = int(os.getenv('JWT_ACCESS_TOKEN_EXPIRES', 60 * 60)) #defaults to 1 hour
-jwt_refresh_token_expires = int(os.getenv('JWT_REFRESH_TOKEN_EXPIRES', 60 * 60 * 24)) #defaults to 1 day
+jwt_access_token_expires = int(os.getenv('JWT_ACCESS_TOKEN_EXPIRES', 60 * 15)) #defaults to 15 minutes
+jwt_refresh_token_expires = int(os.getenv('JWT_REFRESH_TOKEN_EXPIRES', 60 * 60 * 24 * 30)) #defaults to 30 days
 
 
-def create_token(typ='at+jwt', **kwargs):
+def create_token(user, application, typ='at+jwt', **kwargs):
     headers = {
         "typ": typ
     }
     now = time.time()
     payload = {
         "iss": 'auth.rykroon.com', #use ENV var
-        "aud": '', # the application uuid
-        "sub": '', # the user uuid
+        "aud": application.pk,
+        "sub": user.pk,
         "exp": now + jwt_access_token_expires,
         "iat": now,
         "jti": str(uuid.uuid4())
