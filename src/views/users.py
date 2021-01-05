415cf61b17d1535d3dfade19746851c7d2abd399
Ryan Kroon <rykroon.tech@gmail.com>
2021-01-04 19:31:11 -0500

    added UserView

---
+++ src/views/users.py
@@ -0,0 +1,16 @@
+from .base import BaseView
+
+
+class UserView(BaseView):
+
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+
+    def put(self):
+        pass
+
+    def delete(self):
+        pass
