0a0d5823bc977d776be2ed4266430c8a546c50df
Ryan Kroon <rykroon.tech@gmail.com>
2021-01-04 19:35:27 -0500

    added an is_active field to the application model

--- src/models/applications.py
+++ src/models/applications.py
@@ -1,10 +1,11 @@
-from mongoengine.fields import StringField, UUIDField
+from mongoengine.fields import BooleanField, StringField, UUIDField
 from .base import BaseDocument
 
 
 class Application(BaseDocument):
     name = StringField(required=True)
     description = StringField()
+    is_active = BooleanField(default=True)
     client_id = UUIDField(required=True)
 
     
