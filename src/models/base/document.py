23b0e5b414fe5994dd41d4e90811b195560cb351
Ryan Kroon <rykroon.tech@gmail.com>
2021-01-11 22:18:52 -0500

    added BaseQuerySet model with get_or_404 and first_or_404 methods

--- src/models/base/document.py
+++ src/models/base/document.py
@@ -1,12 +1,35 @@
 from datetime import datetime
 import uuid
-from mongoengine import Document
+from mongoengine import Document, QuerySet
+from mongoengine.errors import DoesNotExist
 from mongoengine.fields import DateTimeField, ObjectIdField, UUIDField
+from werkzeug.exceptions import NotFound
+
+
+class BaseQuerySet(QuerySet):
+    def get_or_404(self, *args, **kwargs):
+        """
+        Get a document and raise a 404 Not Found error if it doesn't
+        exist.
+        """
+        try:
+            return self.get(*args, **kwargs)
+        except DoesNotExist:
+            message = kwargs.get("message", None)
+            raise NotFound(message)
+
+    def first_or_404(self, message=None):
+        """Same as get_or_404, but uses .first, not .get."""
+        obj = self.first()
+        if not obj:
+            raise NotFound(message)
+        return obj
 
 
 class BaseDocument(Document):
     meta = {
-        'abstract': True
+        'abstract': True,
+        'queryset_class': BaseQuerySet
     }
 
     id = UUIDField(primary_key=True, default=uuid.uuid4)
