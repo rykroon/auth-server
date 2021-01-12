ef22483d939443f56d4c00d0946d00b008385896
Ryan Kroon <rykroon.tech@gmail.com>
2021-01-11 22:17:44 -0500

    added register_api classmethod

--- src/views/base.py
+++ src/views/base.py
@@ -19,8 +19,28 @@ class BaseView(MethodView):
         if choices and param not in choices:
             raise BadRequest("Invalid value for parameter '{}'.".format(param_name))
 
-    def get_document(self, document_type, **kwargs):
-        doc = document_type.objects.filter(**kwargs).first()
+        return param
+
+    def get_document(self, doc_cls, **kwargs):
+        doc = doc_cls.objects.filter(**kwargs).first()
         if doc is None:
-            raise BadRequest("Invalid '{}'.".format(document_type.__name__))
+            raise BadRequest("Invalid '{}'.".format(doc_cls.__name__))
+        return doc
+
+    @classmethod
+    def register_api(cls, blueprint, url, pk='id', pk_type='int', create=False, list_=False, get=False, update=False, delete=False):
+        view_func = cls.as_view(cls.__name__)
+        if create:
+            blueprint.add_url_rule(url, view_func=view_func, methods=['POST'])
+
+        if list_:
+            blueprint.add_url_rule(url, defaults={pk: None}, view_func=view_func, methods=['GET'])
+
+        if get or update or delete:
+            z = zip(('GET', 'UPDATE', 'DELETE'), (get, update, delete))
+            methods = [m for m, b in z if b]
+            blueprint.add_url_rule('{}<{}:{}>'.format(url, pk_type, pk),
+                view_func=view_func,
+                methods=methods
+            )
 
