ef2b8029a5623352de9931d82e557350c54ff44d
Ryan Kroon <rykroon.tech@gmail.com>
2021-01-13 22:38:42 -0500

    removed things I didn't need anymore

--- src/models/base/document.py
+++ src/models/base/document.py
@@ -6,26 +6,6 @@ from mongoengine.fields import DateTimeField, ObjectIdField, UUIDField
 from werkzeug.exceptions import NotFound
 
 
-class BaseQuerySet(QuerySet):
-    def get_or_404(self, *args, **kwargs):
-        """
-        Get a document and raise a 404 Not Found error if it doesn't
-        exist.
-        """
-        try:
-            return self.get(*args, **kwargs)
-        except DoesNotExist:
-            message = kwargs.get("message", None)
-            raise NotFound(message)
-
-    def first_or_404(self, message=None):
-        """Same as get_or_404, but uses .first, not .get."""
-        obj = self.first()
-        if not obj:
-            raise NotFound(message)
-        return obj
-
-
 class BaseDocument(Document):
     meta = {
         'abstract': True,
