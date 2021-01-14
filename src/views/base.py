f8e141ecec8686db2cb2a71418a88e2860b74b62
Ryan Kroon <rykroon.tech@gmail.com>
2021-01-14 08:58:07 -0500

    added APIView

--- src/views/base.py
+++ src/views/base.py
@@ -1,4 +1,4 @@
-from flask import request
+from flask import g, request
 from flask.views import MethodView
 from werkzeug.exceptions import BadRequest
 
@@ -43,4 +43,38 @@ class BaseView(MethodView):
                 view_func=view_func,
                 methods=methods
             )
+
+
+class APIView(MethodView):
+    #inspired by Django Rest Framework
+
+    authentication_classes = []
+    permission_classes = []
+    throttle_classes = []
+
+    def dispatch_request(self):
+        self._perform_authentication()
+        self._check_permissions()
+        self._check_throttles()
+        return super().dispatch_request()
+
+    def _perform_authentication(self):
+        for auth_class in self.authentication_classes:
+            authenticator = auth_class()
+            g.client = authenticator.authenticate()
+            if g.client is not None:
+                return
+
+        self._not_authenticated()
+
+    def _not_authenticated(self):
+        pass
+
+    def _check_permissions(self):
+        pass
+
+    def _check_throttles(self):
+        pass
+
+        
 
