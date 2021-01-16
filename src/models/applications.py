edf8e861ef1b6b5cb8742bbb563eb4dc2ddd2e91
Ryan Kroon <rykroon.tech@gmail.com>
2021-01-16 12:31:40 -0500

    added 'type' field to the application model

--- src/models/applications.py
+++ src/models/applications.py
@@ -2,9 +2,13 @@ from mongoengine.fields import BooleanField, StringField, UUIDField
 from .base import BaseDocument
 
 
+APPLICATION_TYPES = ('NATIVE', 'WEB')
+
+
 class Application(BaseDocument):
     name = StringField(required=True)
     description = StringField()
+    type = StringField(choices=APPLICATION_TYPES)
     is_active = BooleanField(default=True)
     client_id = UUIDField(required=True)
 
