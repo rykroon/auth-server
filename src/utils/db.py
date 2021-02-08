42103f8c1f42cec14a746762d4d5466fe0d9d9fa
Ryan Kroon <rykroon.tech@gmail.com>
2021-02-08 07:56:03 -0500

    store mongodb client in a varaible

--- src/utils/db.py
+++ src/utils/db.py
@@ -5,7 +5,7 @@ import redis
 mongo_db = os.getenv('MONGO_DB')
 mongo_host = os.getenv('MONGO_HOST', 'localhost')
 mongo_port = os.getenv('MONGO_PORT', 27017)
-connect(mongo_db, host=mongo_host, port=mongo_port)
+mongodb_client = connect(mongo_db, host=mongo_host, port=mongo_port)
 
 redis_host = os.getenv('REDIS_HOST')
 redis_pass = os.getenv('REDIS_PASSWORD')
